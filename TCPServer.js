c0b12cc62c52b9781d3c959df1ccfd40bf56e240
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-02-25 23:44:55 -0800

    finished TCP SERVER

--- TCPServer.js
+++ TCPServer.js
@@ -1,1 +1,15 @@
-const net = require('net')
+const net = require('net');
+const strftime = require('strftime');
+//Port number supplied as first command-line argument
+var portNumber= process.argv[2];
+
+var server = net.createServer(function(socket)
+    {
+    
+    //https://github.com/samsonjs/strftime/blob/master/Readme.md
+    var time = strftime('%F %H:%M')
+    
+    //Newline and ended
+    socket.end(time + "\n");
+    })
+server.listen(portNumber);
